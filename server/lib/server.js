76632da84a8a6675eb50b96be53daa5a82b6613b
Michael Aldridge <mikea3113@gmail.com>
2015-04-03 10:31:26 +0100

    part 2 completed - server now working BUT a few niggles to sort out:
    -what is there a blank function in player_server (ask Dan)

--- server/lib/server.js
+++ server/lib/server.js
@@ -7,11 +7,15 @@ var GameServer   = require('./game_server.js');
  * and game server, create the list of colours and initialise a game id
  * @constructor
  */
-function Server() {
+function Server() 
+{
     this.game_id = -1;
-    this.colours = [];
-
+    this.colours = ['Black', 'Blue', 'Green', 'Red', 'White', 'Yellow'];
+	 
     //TODO
+    var self = this;
+   	self.player_server = new PlayerServer();
+   	self.game_server = new GameServer();
 }
 
 /**
@@ -22,16 +26,30 @@ function Server() {
  * @param player_port
  * @param game_port
  */
-Server.prototype.start = function(player_port, game_port) {
+Server.prototype.start = function(player_port, game_port) 
+{
     //TODO
+    var self = this;
+    self.player_server.listen(player_port);
+    self.game_server.listen(game_port);
+    self.game_server.on('initialised', function(data)
+    {
+    	self.game_id = data;
+    });
+    self.register();
+    self.move();
 }
 
 
 /**
  * Function to close down the player server and the game server
  */
-Server.prototype.close = function() {
+Server.prototype.close = function()
+{
     //TODO
+    var self = this;
+    self.player_server.close();
+    self.game_server.close();
 }
 
 
@@ -39,20 +57,81 @@ Server.prototype.close = function() {
  * Function to retrieve the id of the game being played
  * @returns {number|*}
  */
-Server.prototype.gameId = function() {
+Server.prototype.gameId = function() 
+{
     //TODO
+    var self = this;
+    return self.game_id;
 }
 
 
 /**
  * Function to extract the next colour out of the arrays of colours
  * When a colour is extracted, that colour is removed from the list
- * @returns {*}
+ * @returns {colour}
  */
-Server.prototype.getNextColour = function() {
+Server.prototype.getNextColour = function() 
+{
     //TODO
+    var colour =  this.colours[0];
+    this.colours.shift();
+    return colour;
 }
 
+Server.prototype.register = function()
+{
+	var self = this;
+	self.player_server.on('register', function(player,student_id)
+	{
+		var colour = self.getNextColour();
+		var game_id = self.gameId();
+		self.game_server.addPlayer(player, colour, game_id); 
+	});
+}
+
+Server.prototype.move = function()
+{
+	var self = this;
+	self.player_server.on('move', function(player,connection)
+	{
+		self.game_server.makeMove(player, connection);
+	});
+}
 
 module.exports = Server;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
