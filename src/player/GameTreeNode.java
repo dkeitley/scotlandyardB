5db4a26956171327fadad4516a11aaf61a53eab0
Michael Aldridge <mikea3113@gmail.com>
2015-04-23 08:58:37 +0100

    Please ignore

--- src/player/GameTreeNode.java
+++ src/player/GameTreeNode.java
@@ -5,7 +5,8 @@ import java.lang.Math;
 import scotlandyard.*;
 import solution.*;
 
-public class GameTreeNode {
+public class GameTreeNode
+{
 
 	private AIModel model;
 	private Set<GameTreeNode> leafNodes;
@@ -13,28 +14,34 @@ public class GameTreeNode {
 	private double score;
 	private Move move;
 
-	public GameTreeNode(AIModel model) {
+	public GameTreeNode(AIModel model)
+	{
 		this.model = model;
 		leafNodes = new HashSet<GameTreeNode>();
 	}
 
-	public void setScore(double score)  {
+	public void setScore(double score)
+	{
 		this.score = score;
 	}
 
-	public void setLeafNodes(Set<GameTreeNode> leafNodes) {
+	public void setLeafNodes(Set<GameTreeNode> leafNodes)
+	{
 		this.leafNodes = leafNodes;
 	}
 
-	public void addLeafNode(GameTreeNode node) {
+	public void addLeafNode(GameTreeNode node)
+	{
 		leafNodes.add(node);
 	}
 
-	public void setParentNode(GameTreeNode node) {
+	public void setParentNode(GameTreeNode node)
+	{
 		parentNode = node;
 	}
 
-	public setMove(Move move) {
+	public setMove(Move move)
+	{
 		this.move = move;
 	}
 }
